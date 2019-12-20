3c27c0ea37b896b47568fdc97547437a416a6b9a
jiraichi <shanazarvaliyev@gmail.com>
2019-12-20 17:02:48 +0800

    changed styling of the first card in home, completely changed styling ofacademia page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Map, latLng, tileLayer, Layer, marker } from "leaflet";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  map: Map;
+
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
+
+  leafletMap() {
+    this.map = new Map("mapId").setView([3.036376, 101.707362], 18);
+    tileLayer(
+      "http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+      {
+        attribution: "edupala.com © ionic LeafLet"
+      }
+    ).addTo(this.map);
+    marker([3.036376, 101.707362])
+      .addTo(this.map)
+      .bindPopup("Academia")
+      .openPopup();
+  }
 
   constructor() {}
-
+  ngOnInit() {}
 }
