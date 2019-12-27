c6806088a3e8e8bead308cb7efbe74c9d5438bc3
jiraichi <shanazarvaliyev@gmail.com>
2019-12-27 17:50:58 +0800

    adjusted rendering to use api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from "@angular/core";
-import { Map, latLng, tileLayer, Layer, marker } from "leaflet";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
+import { ApiService } from '../api.service';
+import { APULocation } from '../interfaces/locations';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
   map: Map;
+  locations$: Observable<APULocation[]>;
 
+  // LEAFLET MAP
   ionViewDidEnter() {
     this.leafletMap();
   }
 
   leafletMap() {
-    this.map = new Map("mapId").setView([3.036376, 101.707362], 18);
+    this.map = new Map('mapId').setView([3.036376, 101.707362], 18);
     tileLayer(
-      "http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+      'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
       {
-        attribution: "edupala.com © ionic LeafLet"
+        attribution: 'edupala.com © ionic LeafLet'
       }
     ).addTo(this.map);
     marker([3.036376, 101.707362])
       .addTo(this.map)
-      .bindPopup("Academia")
+      .bindPopup('Academia')
       .openPopup();
   }
 
-  constructor() {}
-  ngOnInit() {}
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.locations$ = this.api.getLocations();
+  }
 }
